bee57ffdb74fbeb1ebdd6cb319ab6c8aea0873e8
Linux mint VM <yt8956789@gmail.com>
2019-05-24 23:44:45 +0800

    change file name of testInterp.java to myInterp_test.java

---
+++ project_3/myInterp/myInterp_test.java
@@ -0,0 +1,13 @@
+import org.antlr.runtime.*;
+
+public class myInterp_test {
+	public static void main(String[] args) throws Exception {
+
+      CharStream input = new ANTLRFileStream(args[0]);
+      myInterpLexer lexer = new myInterpLexer(input);
+      CommonTokenStream tokens = new CommonTokenStream(lexer);
+ 
+      myInterpParser Interp = new myInterpParser(tokens);
+      Interp.program();
+	}
+}
---
+++ project_3/myInterp/readme.txt
@@ -0,0 +1,17 @@
+How to use my Interpreter ?
+
+1. build class file of java
+	
+	>$ make
+
+2. Test files
+	
+	>$ make test
+
+3. Clear files produced automatically by "make"
+
+	>$ make clear
+
+4. Run steps above in one command
+
+	>& make run
